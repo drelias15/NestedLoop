18b4492c2caff692dfc24d5a9124b8fbafe623c2
drelias15 <drelias15@gmail.com>
2018-07-17 14:50:31 -0400

    first commit

---
+++ src/NestedLoop.java
@@ -0,0 +1,18 @@
+public class NestedLoop {
+
+
+    public static void main (String[] args){
+
+        int row, column;
+
+        for (row =1; row <=9; row++ ){
+
+            for (column =2; column <= row; column++){
+
+                System.out.print(row);
+            }
+            System.out.println(row);
+        }
+
+    }
+}
